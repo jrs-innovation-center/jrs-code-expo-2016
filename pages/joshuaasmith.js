7452bc4e92fdf5f7fd5b0593636e3c2ee9d62eca
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-12-12 11:44:34 -0500

    fixed errors

--- pages/joshuaasmith.js
+++ pages/joshuaasmith.js
@@ -18,11 +18,11 @@ const joshuaasmith = React.createClass({
                     <link rel="stylesheet" href="https://unpkg.com/tachyons@4.5.5/css/tachyons.min.css"/>
                     <link rel="shortcut icon" href="/static/Josh/39cea2e0fac5be29828e22eae34000f8 copy.png"/>
                 </Head>
-                <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+                <article className="mw5 center bg-white-60 br3 pa2-ns mv3 b--black-10">
                     <div className="tc">
-                        <img src="/static/Josh/josh1.jpg" className="dib ba b--black-05 pa2" title="Joshua Smith Profile Photo"/>
+                        <img src="/static/Josh/josh1.jpg" className="dib" title="Joshua Smith Profile Photo"/>
                         <h1 className="f3 mb2">Joshua Smith</h1>
-                        <h2 className="f5 fw4 gray mt0">Jack Russell Software Coding Schol</h2>
+                        <h2 className="f5 fw4 gray mt0">Jack Russell Software Coding School</h2>
                         <h2 className="f5 fw4 gray mt0">Final Project</h2>
                     </div>
                 </article>
@@ -50,7 +50,7 @@ const joshuaasmith = React.createClass({
                     </div>
                 </article>
                 <hr className="w-90"/>
-                <section className="mw7 center avenir">
+                <section className="mw7 center avenir mt4">
                     <article class="bt bb b--black-10">
                         <div className="flex flex-column flex-row-ns">
                             <div className="pr3-ns mb4 mb0-ns w-100 w-40-ns">
@@ -72,8 +72,10 @@ const joshuaasmith = React.createClass({
                                 <div>
                                     <h2 className="avenir fw4 blue mt0 mb3 tc">Try Pet Duet Today!
                                     </h2>
-                                    <p className=" avenir white-80 measure lh-copy mv0">
-                                        See how Pet Duet can help you keep up-to-date with your pet's health!
+                                    <p className=" avenir white-80 measure lh-copy mv0 tc">
+                                        See how Pet Duet can help you keep up-to-date
+                                        <br></br>
+                                        with your pet's health!
                                     </p>
                                 </div>
                             </div>
@@ -88,7 +90,6 @@ const joshuaasmith = React.createClass({
                         <svg className="dib h2 w2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M8 0C3.58 0 0 3.582 0 8c0 3.535 2.292 6.533 5.47 7.59.4.075.547-.172.547-.385 0-.19-.007-.693-.01-1.36-2.226.483-2.695-1.073-2.695-1.073-.364-.924-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.223 1.873.87 2.33.665.072-.517.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.953 0-.873.31-1.587.823-2.147-.083-.202-.358-1.015.077-2.117 0 0 .672-.215 2.2.82.638-.178 1.323-.266 2.003-.27.68.004 1.364.092 2.003.27 1.527-1.035 2.198-.82 2.198-.82.437 1.102.163 1.915.08 2.117.513.56.823 1.274.823 2.147 0 3.073-1.87 3.75-3.653 3.947.287.246.543.735.543 1.48 0 1.07-.01 1.933-.01 2.195 0 .215.144.463.55.385C13.71 14.53 16 11.534 16 8c0-4.418-3.582-8-8-8"/></svg>
                         <span className="f6 db">GitHub</span>
                     </a>
-
                     <a className="link hover-silver near-black dib mh3 tc" href="https://linkedin.com/in/joshuaasmith17" title="linkedin" target="_blank">
                         <svg className="dib h2 w2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964-1.424 1.96v3.778h-2.37V6H8.51V7.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758.614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377zm1.188 8.68H2.37V6h2.376v7.635zM14.816 0H1.18C.528 0 0 .516 0 1.153v13.694C0 15.484.528 16 1.18 16h13.635c.652 0 1.185-.516 1.185-1.153V1.153C16 .516 15.467 0 14.815 0z" fill-rule="nonzero"/></svg>
                         <span className="f6 db avenir">LinkedIn</span>
@@ -103,7 +104,6 @@ const joshuaasmith = React.createClass({
                         </a>
                     </div>
                 </footer>
-
             </div>
         )
     }
